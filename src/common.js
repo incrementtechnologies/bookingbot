5bf8a08d31a1eb1bfb7270f385d36a775faad017
capstone student <capstonestudent@pn.local>
2020-10-20 14:43:18 +0800

    Button Type Added for Buttons

--- src/common.js
+++ src/common.js
@@ -1,5 +1,6 @@
 export default {
   sidebarMenu: [
+    {accountType: 'ALL', accountStatus: 'ALL', showOnAdmin: true, description: 'Dashboard', icon: 'fa fa-tachometer-alt', path: 'dashboard', flag: false, subMenu: null},
     {accountType: 'MERCHANT', accountStatus: 'ALL', showOnAdmin: false, description: 'Dashboard', icon: 'fa fa-tachometer-alt', path: 'dashboard', flag: false, subMenu: null},
     {accountType: 'RIDER', accountStatus: 'ALL', showOnAdmin: true, description: 'Dashboard', icon: 'fa fa-tachometer-alt', path: 'dashboard', flag: false, subMenu: null},
     {accountType: 'ALL', accountStatus: 'ALL', showOnAdmin: true, description: 'Featured', icon: 'fa fa-tachometer-alt', path: 'featured', flag: false, subMenu: null},
